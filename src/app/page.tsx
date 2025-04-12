2e8ddf7c21f8c80499a4137e9a6d93cba1cb6dce
NIPSTec Dev <develepor001@gmail.com>
2025-04-12 12:26:13 +0000

    add category on main page at top of new arrivals  in formatted

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,8 +7,62 @@ import { useRouter } from 'next/navigation';
 import { Product } from '@/types/product';
 import { getProducts } from '@/services/product-service';
 import { cn } from "@/lib/utils";
+import Link from 'next/link';
 
 const API_BASE_URL = '/api/products';
+
+const categories = [
+  {
+    id: 1,
+    name: 'Lipsticks',
+    imageUrl: 'https://fakestoreapi.com/img/71w3oGhZMYL._AC_UY436_QL65_.jpg',
+  },
+  {
+    id: 2,
+    name: 'Face Masks',
+    imageUrl: 'https://fakestoreapi.com/img/71z3kpMAYsL._AC_UY226_.jpg',
+  },
+  {
+    id: 3,
+    name: 'Eyeliners',
+    imageUrl: 'https://fakestoreapi.com/img/71pWzhdypcL._AC_UL640_QL65_ML3_.jpg',
+  },
+  {
+    id: 4,
+    name: 'Nail Polishes',
+    imageUrl: 'https://fakestoreapi.com/img/61sbMiUnoGL._AC_UL480_QL65_ML3_.jpg',
+  },
+  {
+    id: 5,
+    name: 'Blushes',
+    imageUrl: 'https://fakestoreapi.com/img/71YAIFU48IL._AC_UL640_QL65_ML3_.jpg',
+  },
+  {
+    id: 6,
+    name: 'Foundations',
+    imageUrl: 'https://fakestoreapi.com/img/51eg5OLwL5L._AC_UL640_QL65_ML3_.jpg',
+  },
+  {
+    id: 7,
+    name: 'Concealers',
+    imageUrl: 'https://fakestoreapi.com/img/61pHnJ96oPL._AC_UL640_QL65_ML3_.jpg',
+  },
+  {
+    id: 8,
+    name: 'Bronzers',
+    imageUrl: 'https://fakestoreapi.com/img/81QpkIctqPL._AC_SX679_.jpg',
+  },
+  {
+    id: 9,
+    name: 'Highlighters',
+    imageUrl: 'https://fakestoreapi.com/img/71HblAHs5xL._AC_UY879_-2.jpg',
+  },
+  {
+    id: 10,
+    name: 'Setting Sprays',
+    imageUrl: 'https://fakestoreapi.com/img/71z3kpMAYsL._AC_UY226_.jpg',
+  },
+];
 
 export default function Home() {
   const router = useRouter();
@@ -57,6 +111,20 @@ export default function Home() {
             <Button onClick={() => router.push('/categories')}>Explore Categories</Button>
           </div>
         </section>
+        {/* Categories Section */}
+        <section className="mb-8">
+            <h2 className="text-2xl font-bold mb-4">Categories</h2>
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-5 gap-4">
+                {categories.map(category => (
+                    <Link href={`/category/${category.name}`} key={category.id}>
+                        <div className="p-4 rounded-md shadow-md bg-white hover:bg-gray-100 transition-colors duration-200">
+                            <h3 className="text-lg font-semibold text-gray-800">{category.name}</h3>
+                        </div>
+                    </Link>
+                ))}
+            </div>
+        </section>
+       
       <section className="mb-8">
         <h2 className="text-2xl font-bold mb-4">New Arrivals</h2>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
